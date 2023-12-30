0bfbb1554907de21187b07991f1b08e718838926
vicky <prajwalkawale01@hotmail.com>
2023-12-30 18:32:00 +0530

    practiced with fibonacci number system

---
+++ fibonacci.c
@@ -0,0 +1,25 @@
+// print Nth term of fobonacci
+#include <stdio.h>
+int fibonacci(int n);
+int main()
+{
+    int n = 6;
+    printf("the %d term of fibonacci %d",n, fibonacci(6));
+    return 0;
+}
+
+int fibonacci(int n)
+{
+    if (n == 0)
+    {
+        return 0;
+    }
+    if (n == 1)
+    {
+        return 1;
+    }
+    int fibN1 = fibonacci(n - 1);
+    int fibN2 = fibonacci(n - 2);
+    int term = fibN1 + fibN2;
+    return term;
+}
