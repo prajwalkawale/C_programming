665f2eab4a45ab000e44da713ec879a0e28771c7
vicky <prajwalkawale01@hotmail.com>
2024-01-16 18:32:42 +0530

    practiced wih array

--- pointer_array.c
+++ pointer_array.c
@@ -1,18 +1,29 @@
 #include <stdio.h>
-int main(){
-    int a = 22;
-    int b = 23;
-    int *ptr = &a;
-    int *_ptr=&b;
-    printf("pointer a is %u \n",ptr);
-    *ptr++; // it will increase by 4 bytes 
-    printf("pointer a is %u \n",ptr);
-    ptr--; // it will decrease by 4 bytes 
-    printf("pointer a is %u",ptr);
 
-    // comparison will print true or false in 1's and 0's form 
+int main()
+{
+    int age = 22;
+    int _age = 23;
+    int *ptr = &age;
+    int *_ptr = &_age;
 
-    *ptr = &b;
-    printf("comparision Pointer a with pointer b %u\n",ptr==_ptr);
+    printf("Difference : %u \n", ptr - _ptr);
+    _ptr = &age;
+    printf("Comparision : %u \n", ptr == _ptr);
+
+    int adhar[5];
+    int *ptr1 = &adhar[0];
+    // input
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d index : ",i);
+        scanf("%d",&adhar[i]);
+    }
+    // output
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d index %d \n", i,adhar[i]);
+    }
+
     return 0;
 }
